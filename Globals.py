e02e0176a530681aacff6969b62a59c886473140
xiranw <43282951+xiranw@users.noreply.github.com>
2018-11-28 17:23:02 -0500

    Add files via upload

---
+++ Globals.py
@@ -0,0 +1,10 @@
+# initializes user levels in a global variable
+# CITATION - using global variables across files: 
+
+def __init__():
+    global editedLevels
+    editedLevels = []
+    global levelsToPlay
+    levelsToPlay = []
+    global highScore
+    highScore = 0
